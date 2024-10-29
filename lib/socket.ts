9159efb2a11b056674a24cddbcacf4ecf84d5568
t-rupe <personal@trupe.info>
2024-10-28 22:36:06 -0700

    updated the socket.ts client to use the correct url based on environment

--- lib/socket.ts
+++ lib/socket.ts
@@ -5,8 +5,12 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null;
 
 export const initSocket = (): Socket => {
+  const apiUrl = process.env.NODE_ENV === 'production' 
+    ? process.env.NEXT_PUBLIC_API_URL_PROD 
+    : process.env.NEXT_PUBLIC_API_URL_DEV;
+
   if (!socket) {
-    socket = io("http://localhost:3001", {
+    socket = io(apiUrl, {
       transports: ["websocket"],
       autoConnect: false,
       forceNew: true, // Force new connection each time
